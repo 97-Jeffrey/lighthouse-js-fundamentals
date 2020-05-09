67cfd1aa06d96d784e7c832e5966647e35e04a10
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-05-09 01:21:51 +0000

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -10,3 +10,21 @@ console.log('short sleeves will not cut it ')
 }
 
 console.log('now you are ready to go outside')
+
+
+
+const isCitizen =true;
+const age =25;
+
+if(isCitizen && age>18){
+  console.log('you are eligible to vote.')
+}
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
