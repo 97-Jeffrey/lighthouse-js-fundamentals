114244c7db20c1e59220ca1e3b0c5f10659d7c99
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-05-11 06:20:55 +0000

    just want to test with splice method

--- ice-cream.js
+++ ice-cream.js
@@ -1,4 +1,4 @@
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 
-
-
+iceCreamFlavours.splice(1,2,"advocado","orange")
+console.log(iceCreamFlavours);
